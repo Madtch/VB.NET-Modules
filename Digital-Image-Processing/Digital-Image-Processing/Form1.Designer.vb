ba819d35481afe8b8045878a4a1b2a34b1e5cb7b
Zamzami Ahmad <zamzamiahmd2112@gmail.com>
2026-08-06 23:45:35 +0700

    add imgprocessing-miniproject02

--- Digital-Image-Processing/Digital-Image-Processing/Form1.Designer.vb
+++ Digital-Image-Processing/Digital-Image-Processing/Form1.Designer.vb
@@ -22,110 +22,190 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TxtBrowse = New System.Windows.Forms.TextBox()
+        Me.BtnBrowse = New System.Windows.Forms.Button()
+        Me.OpenImage = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveImage = New System.Windows.Forms.SaveFileDialog()
+        Me.BtnGrayscale = New System.Windows.Forms.Button()
+        Me.BtnSaveGrayscale = New System.Windows.Forms.Button()
+        Me.BtnBiner = New System.Windows.Forms.Button()
+        Me.BtnSaveBiner = New System.Windows.Forms.Button()
+        Me.PicOriginal = New System.Windows.Forms.PictureBox()
+        Me.PicGrayScale = New System.Windows.Forms.PictureBox()
+        Me.PicBiner = New System.Windows.Forms.PictureBox()
+        CType(Me.PicOriginal, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicGrayScale, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicBiner, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(82, 76)
-        Me.TextBox1.Multiline = True
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(301, 353)
-        Me.TextBox1.TabIndex = 0
-        '
-        'TextBox2
-        '
-        Me.TextBox2.Location = New System.Drawing.Point(418, 76)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(301, 353)
-        Me.TextBox2.TabIndex = 0
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Location = New System.Drawing.Point(766, 76)
-        Me.TextBox3.Multiline = True
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(301, 353)
-        Me.TextBox3.TabIndex = 0
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(162, 50)
+        Me.Label1.Location = New System.Drawing.Point(108, 32)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(99, 20)
+        Me.Label1.Size = New System.Drawing.Size(66, 13)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Citra Original"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(511, 50)
+        Me.Label2.Location = New System.Drawing.Point(341, 32)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(117, 20)
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Citra Grayscale"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(866, 50)
+        Me.Label3.Location = New System.Drawing.Point(577, 32)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(83, 20)
+        Me.Label3.Size = New System.Drawing.Size(55, 13)
         Me.Label3.TabIndex = 1
         Me.Label3.Text = "Citra Biner"
         '
-        'TextBox4
+        'TxtBrowse
         '
-        Me.TextBox4.Location = New System.Drawing.Point(82, 452)
-        Me.TextBox4.Multiline = True
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(179, 32)
-        Me.TextBox4.TabIndex = 2
+        Me.TxtBrowse.Location = New System.Drawing.Point(55, 294)
+        Me.TxtBrowse.Margin = New System.Windows.Forms.Padding(2)
+        Me.TxtBrowse.Multiline = True
+        Me.TxtBrowse.Name = "TxtBrowse"
+        Me.TxtBrowse.Size = New System.Drawing.Size(121, 22)
+        Me.TxtBrowse.TabIndex = 2
         '
-        'Button1
+        'BtnBrowse
         '
-        Me.Button1.Location = New System.Drawing.Point(270, 452)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(113, 49)
-        Me.Button1.TabIndex = 3
-        Me.Button1.Text = "Browse"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.BtnBrowse.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.BtnBrowse.Location = New System.Drawing.Point(180, 294)
+        Me.BtnBrowse.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnBrowse.Name = "BtnBrowse"
+        Me.BtnBrowse.Size = New System.Drawing.Size(75, 32)
+        Me.BtnBrowse.TabIndex = 3
+        Me.BtnBrowse.Text = "Browse"
+        Me.BtnBrowse.UseVisualStyleBackColor = False
+        '
+        'OpenImage
+        '
+        Me.OpenImage.FileName = "OpenImage"
+        '
+        'SaveImage
+        '
+        Me.SaveImage.FileName = "SaveImage"
+        '
+        'BtnGrayscale
+        '
+        Me.BtnGrayscale.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.BtnGrayscale.Location = New System.Drawing.Point(279, 294)
+        Me.BtnGrayscale.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnGrayscale.Name = "BtnGrayscale"
+        Me.BtnGrayscale.Size = New System.Drawing.Size(75, 32)
+        Me.BtnGrayscale.TabIndex = 3
+        Me.BtnGrayscale.Text = "GrayScale"
+        Me.BtnGrayscale.UseVisualStyleBackColor = False
+        '
+        'BtnSaveGrayscale
+        '
+        Me.BtnSaveGrayscale.BackColor = System.Drawing.Color.Red
+        Me.BtnSaveGrayscale.Location = New System.Drawing.Point(358, 294)
+        Me.BtnSaveGrayscale.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnSaveGrayscale.Name = "BtnSaveGrayscale"
+        Me.BtnSaveGrayscale.Size = New System.Drawing.Size(75, 32)
+        Me.BtnSaveGrayscale.TabIndex = 3
+        Me.BtnSaveGrayscale.Text = "Save As"
+        Me.BtnSaveGrayscale.UseVisualStyleBackColor = False
+        '
+        'BtnBiner
+        '
+        Me.BtnBiner.BackColor = System.Drawing.SystemColors.MenuHighlight
+        Me.BtnBiner.Location = New System.Drawing.Point(511, 294)
+        Me.BtnBiner.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnBiner.Name = "BtnBiner"
+        Me.BtnBiner.Size = New System.Drawing.Size(75, 32)
+        Me.BtnBiner.TabIndex = 3
+        Me.BtnBiner.Text = "Biner"
+        Me.BtnBiner.UseVisualStyleBackColor = False
+        '
+        'BtnSaveBiner
+        '
+        Me.BtnSaveBiner.BackColor = System.Drawing.Color.Red
+        Me.BtnSaveBiner.Location = New System.Drawing.Point(601, 294)
+        Me.BtnSaveBiner.Margin = New System.Windows.Forms.Padding(2)
+        Me.BtnSaveBiner.Name = "BtnSaveBiner"
+        Me.BtnSaveBiner.Size = New System.Drawing.Size(75, 32)
+        Me.BtnSaveBiner.TabIndex = 3
+        Me.BtnSaveBiner.Text = "Save As"
+        Me.BtnSaveBiner.UseVisualStyleBackColor = False
+        '
+        'PicOriginal
+        '
+        Me.PicOriginal.Location = New System.Drawing.Point(55, 59)
+        Me.PicOriginal.Name = "PicOriginal"
+        Me.PicOriginal.Size = New System.Drawing.Size(200, 206)
+        Me.PicOriginal.TabIndex = 4
+        Me.PicOriginal.TabStop = False
+        '
+        'PicGrayScale
+        '
+        Me.PicGrayScale.Location = New System.Drawing.Point(279, 59)
+        Me.PicGrayScale.Name = "PicGrayScale"
+        Me.PicGrayScale.Size = New System.Drawing.Size(200, 206)
+        Me.PicGrayScale.TabIndex = 4
+        Me.PicGrayScale.TabStop = False
+        '
+        'PicBiner
+        '
+        Me.PicBiner.Location = New System.Drawing.Point(511, 59)
+        Me.PicBiner.Name = "PicBiner"
+        Me.PicBiner.Size = New System.Drawing.Size(200, 206)
+        Me.PicBiner.TabIndex = 4
+        Me.PicBiner.TabStop = False
         '
         'Form1
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1152, 550)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox4)
+        Me.ClientSize = New System.Drawing.Size(768, 358)
+        Me.Controls.Add(Me.PicBiner)
+        Me.Controls.Add(Me.PicGrayScale)
+        Me.Controls.Add(Me.PicOriginal)
+        Me.Controls.Add(Me.BtnBiner)
+        Me.Controls.Add(Me.BtnSaveBiner)
+        Me.Controls.Add(Me.BtnSaveGrayscale)
+        Me.Controls.Add(Me.BtnGrayscale)
+        Me.Controls.Add(Me.BtnBrowse)
+        Me.Controls.Add(Me.TxtBrowse)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.PicOriginal, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicGrayScale, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicBiner, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents Button1 As Button
+    Friend WithEvents TxtBrowse As TextBox
+    Friend WithEvents BtnBrowse As Button
+    Friend WithEvents OpenImage As OpenFileDialog
+    Friend WithEvents SaveImage As SaveFileDialog
+    Friend WithEvents BtnGrayscale As Button
+    Friend WithEvents BtnSaveGrayscale As Button
+    Friend WithEvents BtnBiner As Button
+    Friend WithEvents BtnSaveBiner As Button
+    Friend WithEvents PicOriginal As PictureBox
+    Friend WithEvents PicGrayScale As PictureBox
+    Friend WithEvents PicBiner As PictureBox
 End Class
--- Digital-Image-Processing/Digital-Image-Processing/Form1.vb
+++ Digital-Image-Processing/Digital-Image-Processing/Form1.vb
@@ -1,3 +1,91 @@
-﻿Public Class Form1
+﻿Imports System.Drawing.Imaging 'library untuk save file image
 
+Public Class Form1
+    Private Sub BtnBrowse_Click(sender As Object, e As EventArgs) Handles BtnBrowse.Click, BtnSaveBiner.Click, BtnBiner.Click, BtnSaveGrayscale.Click, BtnGrayscale.Click
+        With OpenImage
+            .FileName = String.Empty
+            .InitialDirectory = "D:"
+            .Title = "Open Image File"
+            .Filter = "JPG/JPEG/PNG/BMP|*.jpg;*.jpeg;*.png;*.bmp"
+        End With
+
+        Dim result As DialogResult = OpenImage.ShowDialog()
+        If result = Windows.Forms.DialogResult.OK Then
+            Try
+                TxtBrowse.Text = OpenImage.FileName
+                PicOriginal.ImageLocation = OpenImage.FileName
+            Catch ex As Exception
+                MsgBox("Error: " & ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub BtnGrayscale_Click(sender As Object, e As EventArgs) Handles BtnGrayscale.Click
+        If PicOriginal.Image Is Nothing Then
+            MsgBox("Peringatan: Citra original tidak ada !!")
+            Exit Sub
+        End If
+
+        Dim picture As New Bitmap(PicOriginal.Image)
+        Dim x, y, warna As Integer
+        For x = 0 To picture.Width - 1
+            For y = 0 To picture.Height - 1
+                Dim p = picture.GetPixel(x, y)
+                warna = CInt((p.R * 1 + p.G * 1 + p.B * 1) / 3)
+                picture.SetPixel(x, y, Color.FromArgb(warna, warna, warna))
+            Next y
+        Next x
+        PicGrayscale.Image = picture
+    End Sub
+
+    Private Sub BtnSaveGrayscale_Click(sender As Object, e As EventArgs) Handles BtnSaveGrayscale.Click
+        If PicGrayscale.Image Is Nothing Then
+            MsgBox("Peringatan: Citra grayscale tidak ada!!")
+            Exit Sub
+        End If
+
+        SaveImage.Filter = "PNG | *.png"
+        If SaveImage.ShowDialog = Windows.Forms.DialogResult.OK Then
+            My.Computer.FileSystem.CreateDirectory("My Pictures")
+            PicGrayscale.Image.Save(SaveImage.FileName, ImageFormat.Png)
+            MsgBox("Citra grayscale tersimpan.")
+        End If
+    End Sub
+
+    Private Sub BtnBiner_Click(sender As Object, e As EventArgs) Handles BtnBiner.Click
+        If PicOriginal.Image Is Nothing Then
+            MsgBox("Peringatan: Citra original tidak ada!!")
+            Exit Sub
+        End If
+
+        Dim picture As New Bitmap(PicOriginal.Image)
+        Dim x, y, warna As Integer
+        For x = 0 To picture.Width - 1
+            For y = 0 To picture.Height - 1
+                Dim p = picture.GetPixel(x, y)
+                warna = CInt((p.R * 1 + p.G * 1 + p.B * 1) / 3)
+                If warna > 150 Then 'treshold (ambang batas)
+                    warna = 255
+                Else
+                    warna = 0
+                End If
+                picture.SetPixel(x, y, Color.FromArgb(warna, warna, warna))
+            Next y
+        Next x
+        PicBiner.Image = picture
+    End Sub
+
+    Private Sub BtnSaveBiner_Click(sender As Object, e As EventArgs) Handles BtnSaveBiner.Click
+        If PicBiner.Image Is Nothing Then
+            MsgBox("Peringatan: Citra biner tidak ada!!")
+            Exit Sub
+        End If
+
+        SaveImage.Filter = "PNG | *.png"
+        If SaveImage.ShowDialog = Windows.Forms.DialogResult.OK Then
+            My.Computer.FileSystem.CreateDirectory("My Pictures")
+            PicBiner.Image.Save(SaveImage.FileName, ImageFormat.Png)
+            MsgBox("Citra biner tersimpan.")
+        End If
+    End Sub
 End Class
